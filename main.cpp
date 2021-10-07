e62a960becdd9abf6cc64eca27d9e7d07a2071d6
Tony Lang <TonylsLame@users.noreply.github.com>
2021-10-06 19:53:57 -0700

    main changes, discard

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include "person.h"
 #include "pqueuearray.h"
-<<<<<<< HEAD
-
-=======
->>>>>>> 4ec63397d78799ad5bd6851f18c5ad2a0fa230d7
 
 int main () {
-    PQueueArray<Person> pQueue[10];
+    PQueueArray<Person> pQueue(10);
     //pQueue.enqueue(ethan);
     //std::cout << "Front of queue: " << pQueue.peekFront() << std::endl;
 
