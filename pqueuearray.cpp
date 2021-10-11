f253f6bf77fde41622ecca96c24145aa806cf190
Tony Lang <TonylsLame@users.noreply.github.com>
2021-10-10 18:38:13 -0700

    PQueueArray now functions circularly

--- pqueuearray.cpp
+++ pqueuearray.cpp
@@ -13,31 +13,67 @@ PQueueArray<ItemType>::PQueueArray(int newSize) : QueueArray<ItemType>(newSize) 
 template<class ItemType>
 PQueueArray<ItemType>::PQueueArray(const PQueueArray& oldQueue) : QueueArray<ItemType>(oldQueue) {}
 
+template<class ItemType>
+bool PQueueArray<ItemType>::enqueue(const ItemType& newEntry) {
+    if (this->isEmpty()) {
+        this->queue[this->toCircIndex(this->frontIndex)] = newEntry;
+        this->backIndex++;
+        this->itemCount++;
+        return true;
+    }
+    else if (!this->isFull()) {
+        for (int i = this->toCircIndex(this->frontIndex); i <= this->toCircIndex(this->backIndex); i++) {
+            if (newEntry <= this->queue[i]) {
+                for (int j = this->backIndex+1; j > i; j--) {
+                    this->queue[j] = this->queue[j-1];
+                }
+                this->queue[i] = newEntry;
+                i = this->backIndex + 1;
+            }
+            else if (newEntry > this->queue[i] && i == this->backIndex) {
+                this->queue[i] = newEntry;
+            }
+        }
+        this->backIndex++;
+        this->itemCount++;
+        return true;
+    }
+
+
+    else
+        return false;
+}
+
 // template<class ItemType>
 // bool PQueueArray<ItemType>::enqueue(const ItemType& newEntry) {
-//     if (this->isEmpty()) {
-//         this->queue[this->frontIndex] = newEntry;
-//         this->backIndex++;
-//         this->itemCount++;
+//     using QueueArray<ItemType>::frontIndex, QueueArray<ItemType>::backIndex, 
+//     QueueArray<ItemType>::itemCount, QueueArray<ItemType>::maxSize;
+//     if (isEmpty()) {
+//         // adds newEntry to the front if no other items present
+//         queue[this->frontIndex] = newEntry;
+//         backIndex++;
+//         itemCount++;
 //         return true;
-//     }
-//     else if (!this->isFull()) {
-//         for (int i = this->frontIndex; i <= this->backIndex; i++) {
-//             if (newEntry <= this->queue[i]) {
-//                 for (int j = this->backIndex+1; j > i; j--) {
-//                     this->queue[j] = this->queue[j-1];
+//     } // ends if
+//     else if (!isFull()) {
+//         // iterates through queue to find a value newEntry is less than/equal to
+//         for (int i = frontIndex; i <= backIndex; i++) {
+//             if (newEntry <= queue[i]) {
+//                 for (int j = backIndex+1; j > i; j--) {
+//                     // iterates from back + 1 until i to shift items down
+//                     queue[j] = queue[j-1];
 //                 }
-//                 this->queue[i] = newEntry;
-//                 i = this->backIndex + 1;
+//                 queue[i] = newEntry;
+//                 i = backIndex + 1;
 //             }
-//             else if (newEntry > this->queue[i] && i == this->backIndex) {
-//                 this->queue[i] = newEntry;
+//             else if (newEntry > queue[i] && i == backIndex) {
+//                 queue[i] = newEntry;
 //             }
 //         }
-//         this->backIndex++;
-//         this->itemCount++;
+//         backIndex++;
+//         itemCount++;
 //         return true;
-//     }
+//     } // ends else if
 
 
 //     else
@@ -45,46 +81,10 @@ PQueueArray<ItemType>::PQueueArray(const PQueueArray& oldQueue) : QueueArray<Ite
 // }
 
 template<class ItemType>
-bool PQueueArray<ItemType>::enqueue(const ItemType& newEntry) {
-    using QueueArray<ItemType>::frontIndex, QueueArray<ItemType>::backIndex, 
-    QueueArray<ItemType>::itemCount, QueueArray<ItemType>::maxSize;
-    if (isEmpty()) {
-        // adds newEntry to the front if no other items present
-        queue[this->frontIndex] = newEntry;
-        backIndex++;
-        itemCount++;
-        return true;
-    } // ends if
-    else if (!isFull()) {
-        // iterates through queue to find a value newEntry is less than/equal to
-        for (int i = frontIndex; i <= backIndex; i++) {
-            if (newEntry <= queue[i]) {
-                for (int j = backIndex+1; j > i; j--) {
-                    // iterates from back + 1 until i to shift items down
-                    queue[j] = queue[j-1];
-                }
-                queue[i] = newEntry;
-                i = backIndex + 1;
-            }
-            else if (newEntry > queue[i] && i == backIndex) {
-                queue[i] = newEntry;
-            }
-        }
-        backIndex++;
-        itemCount++;
-        return true;
-    } // ends else if
-
-
-    else
-        return false;
-}
-
-template<class ItemType>
 PQueueArray<ItemType>::~PQueueArray() {
-    using QueueArray<ItemType>::isEmpty, QueueArray<ItemType>::dequeue;
-    while(!isEmpty()) {
-        dequeue();
+    //using QueueArray<ItemType>::isEmpty, QueueArray<ItemType>::dequeue;
+    while(!this->isEmpty()) {
+        this->dequeue();
     }
 }
 #endif
